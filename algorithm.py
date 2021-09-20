2f8e24f8e6800f59f877b5c09033946776167199
anusoorya <89992970+anusoorya@users.noreply.github.com>
2021-09-20 20:59:39 +0530

    algorithm.py
    algorithm.py

---
+++ algorithm.py
@@ -0,0 +1,17 @@
+list=['10','1','8','3','6','5','4','7','x','y']
+X=list.index('x')
+if (X%2)==0:
+    X=(X-2)-2
+    print(X)
+else:
+    (X%2) !=0
+    x=(X-2)+2
+    print (x)
+Y=list.index('y')
+if (Y%2)==0:
+    Y=(Y-2)-2
+    print(Y)
+else:
+    (Y%2)!=0
+    Y=(Y-2)+2
+    print(Y)
